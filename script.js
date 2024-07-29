d762996e3361b1fb037d86ad7b87a6f44638187f
Mbuvi1998 <mbuvidan67@gmail.com>
2024-07-29 08:59:43 +0300

    added changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Select DOM Elements
-    const addButton = document.getElementById('add-task');
+    const addButton = document.getElementById('Add Task');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
 
